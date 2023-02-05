33deee300e3df5f68f6f974950b524f045147737
Sabir Walid Abdurahman <116920447+SabirWalid@users.noreply.github.com>
2023-02-05 20:14:12 +0200

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "python is an interpreted, interactive, object-oriented programming with python
+str = "python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
 str = str[38:66].strip() + str[105:111] + str[0:6].strip()
 print(str)
