d4b63f281c28b1ec2f99552fefeb4a1eedc86c6e
Sabir Walid Abdurahman <116920447+SabirWalid@users.noreply.github.com>
2023-02-07 13:38:57 +0200

    Update 12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for n in rang
+    for n in range(1, 101):
         if n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz", end=" ")
         elif n % 3 == 0:
             print("Fizz", end=" ")
         elif n % 5 == 0:
             print("Buzz", end=" ")
-        else:    
+        else:
             print(n, end=" ")
