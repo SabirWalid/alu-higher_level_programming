18172f813497f53511c8c2c44de79fd94ca1f22f
SabirWalid <s.abdurahma@alustudent.com>
2023-02-22 04:42:15 -0800

    0-add.py

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# import the add() function from add_o.py
+from add_0 import add
+
+# define a and b
+        a = 1
+        b = 2
+
+# call the add() function with a and b as arguments, and print the result
+        print("{} + {} = {}".format(a, b, add(a, b)))
