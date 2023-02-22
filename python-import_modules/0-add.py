958ce6e70f50d10695faaa0433fabe89e10812ab
SabirWalid <s.abdurahma@alustudent.com>
2023-02-22 14:42:31 +0200

    0-add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# import the add() function from add_o.py
 from add_0 import add
 
-# define a and b
-        a = 1
-        b = 2
+a = 1
+b = 2
 
-# call the add() function with a and b as arguments, and print the result
-        print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == '__main__':
+    print("{} + {} = {}".format(a, b, add(a, b)))
