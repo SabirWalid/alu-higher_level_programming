dfc77088e05a232403f2c0c10bd026d0886b7907
Sabir Walid Abdurahman <116920447+SabirWalid@users.noreply.github.com>
2023-02-25 23:00:06 +0200

    Update 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # use indexing to get the first 2 integers of each tuple, or 0 if the tuple is too small
-    a1 = tuple_a[0] if len(tuple_a) >= 1 else 0
-    a2 = tuple_a[1] if len(tuple_a) >= 2 else 0
-    b1 = tuple_b[0] if len(tuple_b) >= 1 else 0
-    b2 = tuple_b[1] if len(tuple_b) >= 2 else 0
-
-    # add the corresponding integers and return a tuple with the result
-    return (a1 + b1, a2 + b2)
+    if len(tuple_a) == 0:
+        tuple_a += (0, 0)
+    elif len(tuple_a) == 1:
+        tuple_a += (0,)
+    elif len(tuple_a) > 2:
+        tuple_a = tuple_a[:2]
+    if len(tuple_b) == 0:
+        tuple_b += (0, 0)
+    elif len(tuple_b) == 1:
+        tuple_b += (0,)
+    elif len(tuple_b) > 2:
+        tuple_b = tuple_b[:2]
+    first_element = tuple_a[0] + tuple_b[0]
+    second_element = tuple_a[1] + tuple_b[1]
+    return tuple((first_element, second_element))
