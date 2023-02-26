9718d5c07e22a8de38914426c1704d0f79ae82b3
SabirWalid <s.abdurahma@alustudent.com>
2023-02-26 12:16:53 +0200

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        print()
+    # Create a new matrix of same size as input matrix with all elements initialized to 0
+    result = [[0 for j in range(len(matrix[0]))] for i in range(len(matrix))]
+    
+    # Iterate over the input matrix and compute the square of each integer
+    # Store the result in the corresponding location in the new matrix
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            result[i][j] = matrix[i][j] ** 2
+    
     return [[element * element for element in row] for row in matrix]t
