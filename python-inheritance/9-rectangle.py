6a1c6d975ff89fe170b43b5eecfa8cd2b2dc0d71
SabirWalid <s.abdurahma@alustudent.com>
2023-03-23 13:55:38 +0200

    9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Inherits BaseGeometry class documented now"""
 
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-    """Task based on 8-rectangle.py"""
-
+from 7-base_geometry import BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Class Rectangle that inherits from BaseGeometry"""
@@ -18,20 +15,8 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Returns [Rectangle] <width>/<height>."""
-
         return str("[Rectangle] {}/{}".format(self.__width, self.__height))
 
     def area(self):
         """Area of rectangle"""
-
         return self.__width * self.__height
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricin
