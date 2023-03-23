55a031f84ce2699549f6d1198e4622742db02d8c
SabirWalid <s.abdurahma@alustudent.com>
2023-03-23 05:08:37 +0200

    0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""List of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Retur-n attributes
+    Args :
+        - object to get attributes
+    """
+
+    return dir(obj)
