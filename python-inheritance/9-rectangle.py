d34ae6d3dcfd901e14772af4f5f62b05d5e62043
SabirWalid <s.abdurahma@alustudent.com>
2023-03-23 13:39:19 +0200

    9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -6,7 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectangle"""
+    """Class Rectangle that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
         """Initialize data."""
