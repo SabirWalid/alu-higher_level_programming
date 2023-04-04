34ad2920fec187f2f5f93b33ab269c3f29eccf08
SabirWalid <s.abdurahma@alustudent.com>
2023-04-04 18:14:04 +0200

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Take the URL as the first argument to the script
-url="$1"
-
-# Send a request to the URL and store the response in a variable
-response=$(curl -sI "$url")
-
-# Use grep to extract the Content-Length header from the response
-# and awk to extract the size of the body in bytes
-size=$(echo "$response" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r')
-
-# Display the size of the body in bytes
-echo "Size of the body in bytes: $size"
+# Script that shows the Content-Length from a HTTP request
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
