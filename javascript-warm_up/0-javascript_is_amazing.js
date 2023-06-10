76bc74d1b9d26118ae83a6b2fbadf70b92559091
SabirWalid <s.abdurahma@alustudent.com>
2023-06-10 15:29:38 +0200

    JavaScript files

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/node
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
-=======
-#!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
->>>>>>> 8dbf46ee33e2cac397d1c8272c7374b2ddfb743a
