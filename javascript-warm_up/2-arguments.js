f7fbfc3549d9d6b7670df0a64dbdeb1e285c6c34
SabirWalid <s.abdurahma@alustudent.com>
2023-06-10 15:55:26 +0200

    2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-    console.log('No arguments');
+    console.log("No arguments");
 } else if (args.length === 1) {
-    console.log('Argument found');
+    console.log("Argument found");
 } else {
-    console.log('Arguments found');
+    console.log("Arguments found");
 }
