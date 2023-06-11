a3c1ff27318153cdc7498bd13bd100fad54388b2
SabirWalid <s.abdurahma@alustudent.com>
2023-06-11 19:25:34 +0200

    11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,16 +4,16 @@ let biggest = 0;
 let i;
 const arrayNumbers = [];
 
-for (i = 2; i < ProcessingInstruction.argv.lenght; i++) {
+for (i = 2; i < process.argv.length; i++) {
   if (Number.isNaN(parseInt(process.argv[i])) === false) {
     arrayNumbers[i - 2] = parseInt(process.argv[i]);
   }
 }
 
-if (arrayNumbers.lenght > 1) {
+if (arrayNumbers.length > 1) {
   biggest = Math.max.apply(null, arrayNumbers);
   i = arrayNumbers.indexOf(biggest);
-  arrayNumber[i] = -Infinity;
+  arrayNumbers[i] = -Infinity;
   biggest = Math.max.apply(null, arrayNumbers);
 }
 
