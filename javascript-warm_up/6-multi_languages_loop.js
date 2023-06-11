aab213796899039e01201d142a23b92a970f9f15
SabirWalid <s.abdurahma@alustudent.com>
2023-06-11 15:33:23 +0200

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arr = ['Cis fun', 'Python is cool', 'JavaScript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
