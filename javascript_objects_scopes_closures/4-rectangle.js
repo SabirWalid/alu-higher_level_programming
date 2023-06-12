920c24d78d0492f81a42239a772da26bef6f993e
SabirWalid <s.abdurahma@alustudent.com>
2023-06-12 19:59:14 +0200

    4-rectangle.js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,43 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor (w, h) {
+  constructor(w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
-      this.heigth = h;
+      this.height = h;
+    } else {
+      // if w or h is not a positive integer or equal to 0, create an empty object
+      this.width = 0;
+      this.height = 0;
     }
   }
 
-  print () {
+  print() {
+    if (this.width === 0 || this.height === 0) {
+      return;
+    }
+
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      console.log("X".repeat(this.width));
     }
   }
 
-  rotate () {
-    const temp = this.height;
-    this.height = this.width;
-    this.width = temp;
+  rotate() {
+    if (this.width === 0 || this.height === 0) {
+      return;
+    }
+
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
   }
 
-  double () {
-    this.height = this.height * 2;
+  double() {
+    if (this.width === 0 || this.height === 0) {
+      return;
+    }
+
     this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 };
