2b123569bf90d3f2c1379bb83cb2cb468c165a21
SabirWalid <s.abdurahma@alustudent.com>
2023-06-13 13:43:50 +0200

    1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const filepath = process.argv[2];
 const stringTowrite = process.argv[3];
 
-fs.writeFileSync(filePath, stringTowrite, 'utf-8', function (error) {
-	if (error) {
-		console.error(error);
-	}
+fs.writeFileSync(filepath, stringTowrite, 'utf-8', function (error) {
+  if (error) {
+    console.error(error);
+  }
 });
