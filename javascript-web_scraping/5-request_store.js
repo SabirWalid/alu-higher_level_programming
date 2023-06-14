e7d09c3afefb819fea7e7a87e78a14f4697ec8f3
SabirWalid <s.abdurahma@alustudent.com>
2023-06-14 21:24:38 +0200

    5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const fs = require('fs');
 
-request(process.arg[2], function (err, response, body) {
+request(process.argv[2], function (err, response, body) {
   if (err == null) {
     fs.writeFileSync(process.argv[3], body);
   }
